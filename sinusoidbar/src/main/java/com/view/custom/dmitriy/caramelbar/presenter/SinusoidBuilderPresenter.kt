dd16493fd280aaea01415e91cec55c4e8473a8cb
@d.nesterov <d.nesterov@s-e.su>
2018-01-26 17:31:36 +0300

    Fixed grammatical errors

--- sinusoidbar/src/main/java/com/view/custom/dmitriy/caramelbar/presenter/SinusoidBuilderPresenter.kt
+++ sinusoidbar/src/main/java/com/view/custom/dmitriy/caramelbar/presenter/SinusoidBuilderPresenter.kt
@@ -26,7 +26,7 @@ class SinusoidBuilderPresenter : SinusoidBuilderView {
      * работает анологично интегралу, по dx бертся f(y) = A(dx*sin(y))sin(w*t+fi)
      * @param halfY координата центра [SinusoidBar] по оси Y
      * @param halfX координата центра [SinusoidBar] по оси X
-     * @param runnableCoordinateY деффиринциал колебания, пересчет деффиринциала необходимо выполнять в [Runnable]
+     * @param runnableCoordinateY дифференциал колебания, пересчет деффиринциала необходимо выполнять в [Runnable]
      * @param objectSize размер отрисованных объектов
      */
     fun sinusoidStartBuild(halfX: Int, halfY: Int, runnableCoordinateY: Float, objectSize: Float):  Path = equationBuilder(halfX, halfY, runnableCoordinateY, objectSize)
@@ -38,7 +38,7 @@ class SinusoidBuilderPresenter : SinusoidBuilderView {
      * работает анологично интегралу, по dx бертся f(y) = A(dx*sin(y))sin(w*t+fi)
      * @param halfY координата центра [SinusoidBar] по оси Y
      * @param halfX координата центра [SinusoidBar] по оси X
-     * @param runnableCoordinateY деффиринциал колебания, пересчет деффиринциала необходимо выполнять в [Runnable]
+     * @param runnableCoordinateY дифференциал колебания, пересчет дифференциала необходимо выполнять в [Runnable]
      * @param objectSize размер отрисованных объектов
      */
     private fun equationBuilder(halfX: Int, halfY: Int, runnableCoordinateY: Float, objectSize: Float): Path {
@@ -54,17 +54,17 @@ class SinusoidBuilderPresenter : SinusoidBuilderView {
      * Уравнение вынужденного колебания
      * @param halfY координата центра [SinusoidBar] по оси Y
      * @param halfX координата центра [SinusoidBar] по оси X
-     * @param dx деффиринциал колебания
-     * @param runnableCoordinateY деффиринциал колебания, пересчет деффиринциала необходимо выполнять в [Runnable]
+     * @param dx дифференциал колебания
+     * @param runnableCoordinateY дифференциал колебания, пересчет дифференциал необходимо выполнять в [Runnable]
      */
     private fun equationDampedOscillation(halfY: Int, dx: Int, halfX: Int, runnableCoordinateY: Float) =
             halfY + amplitudeDumpedOscillation(dx, halfX, runnableCoordinateY) * sin((dx - halfX) + cos(runnableCoordinateY) * 2)
 
             /**
              * Амплитуда вынужденного колебания
-             * @param dx деффиринциал колебания
+             * @param dx дифференциал колебания
              * @param halfX координата центра [SinusoidBar] по оси X
-             * @param runnableCoordinateY деффиринциал колебания, пересчет деффиринциала необходимо выполнять в [Runnable]
+             * @param runnableCoordinateY дифференциал колебания, пересчет дифференциала необходимо выполнять в [Runnable]
              */
             private fun amplitudeDumpedOscillation(dx: Int, halfX: Int, runnableCoordinateY: Float) =
                     ((dx - halfX) * sin(-runnableCoordinateY))
@@ -73,23 +73,23 @@ class SinusoidBuilderPresenter : SinusoidBuilderView {
     /**
      * Уравнение затухающего колебания
      * @param halfY координата центра [SinusoidBar] по оси Y
-     * @param dx деффиринциал колебания
-     * @param runnableCoordinateY деффиринциал колебания, пересчет деффиринциала необходимо выполнять в [Runnable]
+     * @param dx дифференциал колебания
+     * @param runnableCoordinateY дифференциал колебания, пересчет дифференциал необходимо выполнять в [Runnable]
      */
     private fun equationForcedOscillation(halfY: Int, dx: Int, runnableCoordinateY: Float) =
             halfY + amplitudeForcedOscillation(dx, runnableCoordinateY) * sin(dx.toFloat() + cos(runnableCoordinateY) * 2)
 
             /**
              * Амплитуда затухающего колебания
-             * @param dx деффиринциал колебания
-             * @param runnableCoordinateY деффиринциал колебания, пересчет деффиринциала необходимо выполнять в [Runnable]
+             * @param dx дифференциал колебания
+             * @param runnableCoordinateY дифференциал колебания, пересчет дифференциала необходимо выполнять в [Runnable]
              */
             private fun amplitudeForcedOscillation(dx: Int, runnableCoordinateY: Float) =
                     (dx * sin(runnableCoordinateY))
 
     /**
      * Фазовый сдвиг для затухающего колебания
-     * @param dx деффиринциал колебания
+     * @param dx дифференциал колебания
      * @param halfX координата центра [SinusoidBar] по оси X
      */
     private fun phaseShift(dx: Int, halfX: Int) = dx.toFloat() + halfX - density * 2
